475d64c048812a8d1eb162f53fce839346008393
Rustam Nigamatullin <rust2409@ya.ru>
2023-03-22 00:43:46 +0500

    refact: добавить бургер

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,23 +34,23 @@ function Header() {
   }
 
   return (
-    <header className={`header page__header ${isActiveIcon && 'header__sidebar'}`}>
-      <div class="header__row">
-        <a href="#" className="header__logo"></a>
-        <button className={`header__togle-sidebar ${contextApp.loggedIn ? 'header__togle-sidebar_active' : 'header__togle-sidebar_disactive'}`} onClick={handleClickSidebar}>
-          <div className="header__icon">
-            <span className={`header__icon-line-top ${isActiveIcon ? 'header__icon-line-top_active' : ''}`} />
-            <span className={`header__icon-line-middle  ${isActiveIcon ? 'header__icon-line-middle_active' : ''}`} />
-            <span className={`header__icon-line-bottom  ${isActiveIcon ? 'header__icon-line-bottom_active' : ''}`} />
-          </div>
-        </button>
-      </div>
+    <header className={`header page__header ${isActiveIcon && contextApp.loggedIn && 'header__sidebar'}`}>
+
+      <a href="#" className="header__logo"></a>
+      <button className={`header__togle-sidebar ${contextApp.loggedIn ? 'header__togle-sidebar_active' : ''}`} onClick={handleClickSidebar}>
+        <div className="header__icon">
+          <span className={`header__icon-line-top ${isActiveIcon ? 'header__icon-line-top_active' : ''}`} />
+          <span className={`header__icon-line-middle  ${isActiveIcon ? 'header__icon-line-middle_active' : ''}`} />
+          <span className={`header__icon-line-bottom  ${isActiveIcon ? 'header__icon-line-bottom_active' : ''}`} />
+        </div>
+      </button>
+
       {contextApp.loggedIn ? '' : <Link className="header__link" to={path}>{linkText}</Link>}
 
-      <div className={`header__menu ${isActiveIcon ? 'header__menu_active' : ''} `}>
+      {contextApp.loggedIn && <div className={`header__menu ${isActiveIcon ? 'header__menu_active' : ''} `}>
         <span className="header__email">{contextApp.email}</span>
         {!contextApp.loggedIn ? '' : <button className="header__btn-out" type="text" onClick={handleClickSignOut}>Выйти</button>}
-      </div>
+      </div>}
     </header>
   )
 }
